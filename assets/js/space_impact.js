8d45e650a7b88387ac46656927b97b9de79edeb1
Silver_Roi Ramos <silverroiramos13@gmail.com>
2022-11-25 13:55:26 +0800

    js_update

--- assets/js/space_impact.js
+++ assets/js/space_impact.js
@@ -26,42 +26,35 @@ let enemy_drone = () => {
             }
         },
         moveEnemy: function (){
-            let enemy_move_time = (Math.floor(Math.random() * 4) + 1) * 1000;
-            // console.log(this.x, this.y, shoot_time);
-            // enemy_bullets.push({x: this.x-30, y:this.y+5});
+            let enemy_move_time = (Math.floor(Math.random() * 4) + 1) * 35;
             this.x -= ENEMIES_SPEED;
-            if(this.x < -65){
-                enemies.shift();
-            }
 
             const move_enemy = setTimeout(() => {
                 this.moveEnemy();
             }, enemy_move_time);
-     
-            // if(this.health === 0){
-            //     clearTimeout(shoot_bullet); 
-            // }
         }
     };
 } 
 
-let enemy_bumblebee = {
-    title: "bumblebee",
-    x:800,
-    y:100,
-    health: 2,
-    earn_score: 100,
-    shootBullet: function (){
-        let shoot_time = (Math.floor(Math.random() * 4) + 1) * 1000;
-        // console.log(this.x, this.y, shoot_time);
-        enemy_bullets.push({x: this.x-30, y:this.y+5});
+let enemy_bumblebee = () => {
+    return {
+        title: "bumblebee",
+        x:800,
+        y:100,
+        health: 2,
+        earn_score: 100,
+        shootBullet: function (){
+            let shoot_time = (Math.floor(Math.random() * 4) + 1) * 1000;
+            // console.log(this.x, this.y, shoot_time);
+            enemy_bullets.push({x: this.x-30, y:this.y+5});
 
-        const shoot_bullet = setTimeout(() => {
-            this.shootBullet();
-        }, shoot_time);
+            const shoot_bullet = setTimeout(() => {
+                this.shootBullet();
+            }, shoot_time);
 
-        if(this.health === 0){
-            clearTimeout(shoot_bullet); 
+            if(this.health === 0){
+                clearTimeout(shoot_bullet); 
+            }
         }
     }
 }
@@ -76,7 +69,7 @@ let bullet_element_layer = ["bullets", "enemy_bullets"];
 let GAME_HEIGHT =  768;
 let GAME_WIDTH =  1024;
 let SPACESHIP_SPEED = 20;
-let ENEMIES_SPEED = 1;
+let ENEMIES_SPEED = 10;
 let BULLET_MOVE_SPEED = 10; /** 1px/0.33s OR 60px/1s */
 let FPS = 60;
 let GAME_TIMER = Math.floor(1000/FPS);
@@ -97,16 +90,16 @@ document.addEventListener("DOMContentLoaded", () => {
 function gameLoop(){
     moveSpaceshipBullets();
     moveEnemyBullets();
-    moveEnemies();
+    // moveEnemies();
     spaceshipBulletEnemyCollision();
     // spaceshipBulletEnemyBulletCollision();
     spaceshipEnemyCollision();
     spaceshipEnemyBulletCollision();
     displaySpaceship( );
     displayEnemies();
-    // displaySpaceshipBullets();
-    // displayEnemyBullets();
-    displayBullets(bullet_type[0], bullet_array[0], bullet_element_layer[0]);
+    displaySpaceshipBullets();
+    displayEnemyBullets();
+    // displayBullets(bullet_type[0], bullet_array[0], bullet_element_layer[0]);
     // /* spaceship bullets */ expectedFunction(bullet_type, direction, element_layer);
     // /* enemy bullets */ expectedFunction(bullet_type, direction, element_layer);
     displayScore();
@@ -155,13 +148,22 @@ function addDroneEnemy(){
         enemy_spawn.y = (Math.floor(Math.random() * 700) + 1);
         enemies.push(enemy_spawn);
         enemy_spawn.shootBullet();
-        /* enemy_spawn.moveEnemy(); */
+        enemy_spawn.moveEnemy();
         setTimeout(addDroneEnemy, spawn_drone);
     }
 }
 
 function addBumblebeeEnemies(){
-    enemies.push(enemy_bumblebee);
+    if(enemies.length < MAX_ENEMIES){
+        let enemy_spawn = enemy_drone();
+        let spawn_drone = (Math.floor(Math.random() * 4) + 1) * 1000;
+        
+        enemy_spawn.y = (Math.floor(Math.random() * 700) + 1);
+        enemies.push(enemy_spawn);
+        enemy_spawn.shootBullet();
+        enemy_spawn.moveEnemy();
+        setTimeout(addDroneEnemy, spawn_drone);
+    }
 }
 
 function initializeGame(){
@@ -172,23 +174,26 @@ function displayEnemies(){
     let output = "";
     for(enemy_id = 0; enemy_id < enemies.length; enemy_id++){
         output += `<div class='${ enemies[enemy_id].title }' style='top: ${ enemies[enemy_id].y }px; left: ${ enemies[enemy_id].x }px;'></div>`; 
+        if(enemies[enemy_id].x < -65){
+            enemies.shift();
+        }
     } 
     document.getElementById("enemies").innerHTML = output;
 }
 
-function displayBullets(bullet_type, array, element_layer){
-    let output = "";
-    document.getElementById(element_layer).innerHTML = "";
+// function displayBullets(bullet_type, array, element_layer){
+//     let output = "";
+//     document.getElementById(element_layer).innerHTML = "";
     
-    for(bullet_type = 0; bullet_type < array.length; bullet_type++){
-        output += `<div class='${ element_layer }' style='top: ${ array[`${ bullet_type }`].y }px; left: ${ array[bullet_type].x }px;'></div>`;
-        console.log(array[bullet_type]);
-    }
+//     for(bullet_type = 0; bullet_type < array.length; bullet_type++){
+//         output += `<div class='${ element_layer }' style='top: ${ array[`${ bullet_type }`].y }px; left: ${ array[bullet_type].x }px;'></div>`;
+//         console.log(array[bullet_type]);
+//     }
 
-    document.getElementById(element_layer).innerHTML = output;
+//     document.getElementById(element_layer).innerHTML = output;
 
-    console.log(bullets);
-}
+//     console.log(bullets);
+// }
 
 function displaySpaceshipBullets(){
     let output = "";
@@ -232,10 +237,10 @@ function moveEnemyBullets(){
     for (enemy_bullet_id = 0; enemy_bullet_id < enemy_bullets.length; enemy_bullet_id++){
         enemy_bullets[enemy_bullet_id].x -= 10;
 
-        if(enemy_bullets[enemy_bullet_id].x < 0){
-            // enemy_bullets[enemy_bullet_id] = enemy_bullets[enemy_bullets.length-1];
-            // enemy_bullets.pop();
-            enemy_bullets.shift();
+        if(enemy_bullets[enemy_bullet_id].x < -10){
+            enemy_bullets[enemy_bullet_id] = enemy_bullets[enemy_bullets.length-1];
+            enemy_bullets.pop();
+            // enemy_bullets.shift();
         }
     }
 }
